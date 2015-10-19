3f3facbe82cb6bfb64ca0de5ae7bd6d917006710
Jelmer Snoeck <jelmer.snoeck@gmail.com>
2015-10-19 12:52:12 +0100

    Scaler: initial implementation of formation updates.

--- app/models/scaler.rb
+++ app/models/scaler.rb
@@ -17,14 +17,33 @@ class Scaler
     return false if !@slot.scaleable?
     Scaler.delay_until(@slot.to).reset(@slot.id)
 
-    # Get current scale
-    # Save current scale on slot
-    # Scale Heroku
+    formation = heroku.formation.info(app_name, slot.formation_type)
+    slot.set_initial_values(formation["size"].downcase, formation["quantity"])
+    heroku.formation.update(app_name, slot.formation_type, {
+      quantity: slot.formation_quantity,
+      size: slot.formation_size
+    })
   end
 
   def reset!
     return false if !@slot.resetable?
 
-    # Scale Heroku
+    heroku.formation.update(app_name, slot.formation_type, {
+      quantity: slot.formation_initial_quantity,
+      size: slot.formation_initial_size
+    })
+  end
+
+  private
+  def heroku
+    @_heroku ||= PlatformAPI.connect_oauth(ENV['HEROKU_OAUTH'])
+  end
+
+  def slot
+    @slot
+  end
+
+  def app_name
+    ENV['HEROKU_APP_NAME']
   end
 end
