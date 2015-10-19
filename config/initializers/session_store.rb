74a7222e9b9f0156283559a1d03d694f7d8ede03
Jelmer Snoeck <jelmer.snoeck@gmail.com>
2015-10-19 10:25:47 +0100

    Rename to heroku timed scaler.
    Heroku Scheduler is a plugin on Heroku to schedule cronjobs.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_heroku-scheduler_session'
+Rails.application.config.session_store :cookie_store, key: '_heroku_timed_scaler_session'
